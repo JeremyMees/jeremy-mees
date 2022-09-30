0766063e061ceb78e0c6ee40afa7724ffbc37e1d
jeremy <jeremymees123@gmail.com>
2022-09-30 13:48:02 +0200

    Basic start project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
+import svgLoader from 'vite-svg-loader'
 
+export default defineNuxtConfig({
+  components: {
+    global: true,
+    dirs: ['~/components/atoms', '~/components/molecules', '~/components/organisms'],
+  },
+  css: ['~/assets/css/global.css'],
+  modules: ['@pinia/nuxt'],
+  buildModules: ['@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: '~/tailwind.config.js',
+    injectPosition: 0,
+    viewer: false,
+  },
+  vite: {
+    plugins: [svgLoader()],
+  },
 })
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./pages/*.{vue,js}', './layouts/*.{vue,js}', './components/**/*.{vue,js}', './plugins/*.{js,ts}'],
+  darkMode: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        title: ['Abril Fatface', 'sans-serif'],
+        text: ['Lato', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
