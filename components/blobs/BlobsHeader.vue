ca27c636b9b11353a8e62a5e5762c0653f5f17ef
Jeremy <jeremy.mees@appeel.io>
2022-10-19 14:55:04 +0200

    fix(blobs): removed unused font style

--- components/blobs/BlobsHeader.vue
+++ components/blobs/BlobsHeader.vue
@@ -4,9 +4,7 @@ defineEmits(['mouseenter', 'mouseleave'])
 
 <template>
   <div class="flex flex-wrap gap-2 justify-between items-center glass px-4 py-2">
-    <div class="title-small !font-blobs-title" @mouseenter="$emit('mouseenter')" @mouseleave="$emit('mouseleave')">
-      Jeremy Mees
-    </div>
+    <div class="title-small" @mouseenter="$emit('mouseenter')" @mouseleave="$emit('mouseleave')">Jeremy Mees</div>
     <Dropdown @mouseenter="$emit('mouseenter')" @mouseleave="$emit('mouseleave')" />
   </div>
 </template>
